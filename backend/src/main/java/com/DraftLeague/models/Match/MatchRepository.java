cbd1bbb52e5477aef9e207d3e3f3597e7849f6c3
juamorrio <jmoernorios5@gmail.com>
2026-01-30 12:11:46 +0100

    feat: :sparkles: Implement Match functionality with controller, service, and DTOs for played and upcoming matches

---
+++ backend/src/main/java/com/DraftLeague/models/Match/MatchRepository.java
@@ -0,0 +1,8 @@
+package com.DraftLeague.models.Match;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Integer> {
+}

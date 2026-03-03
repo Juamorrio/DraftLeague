e510bd5a053a92b5f5897202aca59ead0aec9e04
juamorrio <jmoernorios5@gmail.com>
2026-03-03 20:05:49 +0100

    fix: :bug: add owner field to app.config.js for EAS

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -4,6 +4,7 @@ const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
 export default {
   name: IS_PROD ? 'DraftLeague' : IS_PREVIEW ? 'DraftLeague (Preview)' : 'DraftLeague (Dev)',
   slug: 'draftleague',
+  owner: 'juanmoreno',
   version: '1.0.0',
   orientation: 'portrait',
   icon: './assets/header/Logo.png',
